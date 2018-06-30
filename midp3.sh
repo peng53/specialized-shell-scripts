46904b738ac95e4a9074c69265436366067b407f
Perry Eng <eng.perry@gmail.com>
2018-06-30 07:56:21 -0500

    Added fq7 & fq8. These are actual versions of 'derived_q' The reason I'm adding these (and not the previous versions) is because specifics were removed. This only works with youtube-dl.

--- midp3.sh
+++ midp3.sh
@@ -5,15 +5,28 @@
 # Would be desirable to now have to write the wav &
 # just "pipe" wav data to lame, but couldn't figure
 # out how; use of ramdisk may mitigated the writing though
-cd /mnt/ramdisk/
-for i in *.mid
-do
-	sf='/usr/share/sounds/sf2/FluidR3_GM.sf2'
-	out=${i%%mid}
-	fluidsynth -F ${out}wav "$sf" "$i"
+#cd /mnt/ramdisk/
+#for i in *.mid
+#do
+#	sf='/usr/share/sounds/sf2/FluidR3_GM.sf2'
+#	out=${i%%mid}
+#	fluidsynth -F ${out}wav "$sf" "$i"
 	# first to wav to ramdisk
 	#lame -v --vbr-new -b 8 -b 64 ${out}wav ${out}mp3
 	#fluidsynth "$sf" "$i" -F | lame -v --vbr-new -b 8 -B 64 ${out}mp3
-	lame -v --vbr-new -b 8 -b 64 ${out}wav '/media/COBY4GB/Music/Midi/'${out}mp3
+#	lame -v --vbr-new -b 8 -b 64 ${out}wav '/media/COBY4GB/Music/Midi/'${out}mp3
 	# then wav to mp3 to player
+#done
+
+mkfifo mywav.wav
+sf='/usr/share/sounds/sf2/FluidR3_GM.sf2'
+out='/mnt/ramdisk'
+for i in *.mid
+do
+	fluidsynth -F mywav.wav $sf "$i" &
+	lame mywav ${out}/`basename ${i} .mid`.mp3
 done
+#for i in *.mid
+#do
+	fluidsynth -F mywav "$sf" "$i" | lame - ${out}/`basename ${i} .mid`.mp3
+#done
