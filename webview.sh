2f19d8e28e14c857f95be1b687e586d949bcdd08
Perry Eng <eng.perry@gmail.com>
2018-10-07 08:17:00 -0500

    Changed webview.sh to use a shell function that takes other exts. Added video extension.
    Upcoming change is using the case statement as it seems more natural
    than checking if postfix removal changed the string (what i'm doing right now)

--- webview.sh
+++ webview.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 [ -z $1 ] && return 1
-for p in jpg jpeg png gif
-do
-	[ $1 != ${1%.$p} ] && exec feh $1
-done
-exec xlinks2 $1
+image_ext='.jpg .jpeg .png .gif'
+video_ext='.mp4 .avi .flv'
+
+has_prefix(){
+	# $1 is the string
+	# $2 are the exts
+	for p in $2
+	do
+		[ $1 != ${1%$p} ] && return 0
+	done
+	return 1
+}
+has_prefix $1 $image_ext && exec feh $1
+has_prefix $1 $video_ext && exec mpv $1
+exec xlinks $1
