5d64a2376ad0c7b64dde1ede612c1724281cbfd2
Perry Eng <eng.perry@gmail.com>
2018-10-07 08:37:13 -0500

    Changed webview.sh to just use a switch case (, which diverges it from webview.py) Also added a 'youtube' test that checks for the https prefix. (the RSS formats its urls like this)

--- webview.sh
+++ webview.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-[ -z $1 ] && return 1
-image_ext='.jpg .jpeg .png .gif'
-video_ext='.mp4 .avi .flv'
+[ "$#" -eq 0 ] && return 1
 
-has_prefix(){
-	# $1 is the string
-	# $2 are the exts
-	for p in $2
-	do
-		[ $1 != ${1%$p} ] && return 0
-	done
-	return 1
-}
-has_prefix $1 $image_ext && exec feh $1
-has_prefix $1 $video_ext && exec mpv $1
-exec xlinks $1
+bin=
+case "$1" in
+	https://www.youtube.com/watch?v=*)
+		bin='./fq8 add'
+	;;
+	*.jpg|*.jpeg|*.png|*.gif) # image exts
+		bin=feh
+	;;
+	*.mp4|*.avi|*.flv) # video exts
+		bin=mpv
+	;;
+	*) # the default option
+		bin=xlinks2
+	;;
+esac
+exec $bin $1
