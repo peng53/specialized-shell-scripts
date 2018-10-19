f0369da9fab855d10254ee4d984920e31d601c07
Perry Eng <eng.perry@gmail.com>
2018-10-18 23:30:24 -0500

    fq.py: 	Mostly replace naive path,file joining with os.path.join. 	Replaced realpath with expanduser. 	Made script more verbose with quality, speed, and other indicators. 	Fixed bug with push_out not checking if old_vid existed before deleting it 	Made no arg possible. 	Made top actually do something. 	Replaced some open lines with the 'with' keyword.

--- fq.py
+++ fq.py
@@ -11,7 +11,7 @@ player_args = "--really-quiet"
 tmp_dir = "/mnt/ramdisk"
 q_name = "mq"
 def_env = {"quality":"36", "speed":"40K"}
-yt_dl = os.path.realpath("~/.local/bin/youtube-dl")
+yt_dl = os.path.expanduser("~/.local/bin/youtube-dl")
 yt_dl_args = ["--no-part","--youtube-skip-dash-manifest","--no-call-home","--no-playlist"]
 
 def get_envvar(varname):
@@ -36,9 +36,9 @@ def remove_top(f_name,f_dir):
 	# remove the first line from f_dir/f_name. (a file).
 	# returns the line that was removed, if it exists, else None
 	tmp_f = tempfile.mktemp(dir=f_dir)
-	os.rename(f_dir.rstrip('/')+'/'+f_name,tmp_f)
+	os.rename(os.path.join(f_dir,f_name),tmp_f)
 	r = None
-	with open(tmp_f,'r') as f, open(f_dir.rstrip('/')+'/'+f_name,'w') as g:
+	with open(tmp_f,'r') as f, open(os.path.join(f_dir,f_name),'w') as g:
 		r = l = f.readline()
 		while l:
 			l = f.read(1024)
@@ -49,30 +49,34 @@ def remove_top(f_name,f_dir):
 def add_bottom(f_name,f_dir,line):
 	l = line.rstrip()
 	if len(l)>0:
-		f = open(f_dir.rstrip('/')+'/'+f_name,'a')
-		f.write(l+'\n')
-		f.close()
+		with open(os.path.join(f_dir,f_name),'a') as f:
+			f.write(l+'\n')
 
 def view_vid(v_dir,v_name="1"):
 	# watch the video that was downloaded.
 	# uses global 'player' and 'player_args'
 	# on linux, this blocks the terminal
 	# returns the 'return code'
-	return subprocess.run([player,player_args,v_dir.rstrip('/')+'/'+v_name]).returncode
+	return subprocess.run([player,player_args,os.path.join(v_dir,v_name)]).returncode
 
 def push_out(v_dir,v_name="1",v_name_old="0"):
 	# displaces last vid by replacing v_name_Old with it
-	t_dir = v_dir.rstrip('/')+'/'
-	if os.path.exists(t_dir+v_name_old):
-		os.remove(t_dir+v_name_old)
-	os.rename(t_dir+v_name,t_dir+v_name_old)
+	old = os.path.join(v_dir,v_name_old)
+	new = os.path.join(v_dir,v_name)
+	if os.path.exists(old):
+		os.remove(old)
+	if os.path.exists(new):
+		os.rename(new,old)
 
 def clear_file(tmp_dir,q_name):
 	# empties a file
-	open(tmp_dir.rstrip('/')+'/'+q_name,'w').close()
+	open(os.path.join(tmp_dir,q_name),'w').close()
 
 def yt_dl_bin(url,v_dir,v_name="1"):
-	cmd = [yt_dl,"-f",get_envvar("quality"),"-r",get_envvar("speed"),url,"-o",v_dir.rstrip('/')+'/'+v_name]+yt_dl_args
+	speed = get_envvar("speed")
+	quality = get_envvar("quality")
+	cmd = [yt_dl,"-f",quality,"-r",speed,url,"-o",os.join.path(v_dir,v_name)]+yt_dl_args
+	print("Downloading video quality={:s} at speed={:s}".format(quality,speed))
 	return subprocess.Popen(cmd)
 
 def try_yt(v_dir,v_name="1"):
@@ -80,9 +84,8 @@ def try_yt(v_dir,v_name="1"):
 		"format":"36",
 		"noplaylist":True,
 		"call_home":False,
-		"filename": v_dir.rstrip('/')+'/'+v_name,
-		"tmpfilename": v_dir.rstrip('/')+'/'+v_name,
-		
+		"filename": os.path.join(v_dir,v_name),
+		"tmpfilename": os.path.join(v_dir,v_name),		
 	}
 
 def case_go(url,resume=False):
@@ -109,6 +112,7 @@ def case_resume(url=None):
 
 def case_flush():
 	clear_file(tmp_dir,q_name)
+	print("Queue was cleared.")
 
 def case_add(url):
 	if url:
@@ -117,26 +121,20 @@ def case_add(url):
 		print("URL was blank.")
 
 def case_top():
-	return top(open(tmp_dir.rstrip('/')+'/'+q_name,'r'))
+	url = top(open(os.path.join(tmp_dir,q_name),'r'))
+	if url:
+		print(url.rstrip())
+	else:
+		print("Queue was empty.")
 
 def case_see():
-	with open(tmp_dir.rstrip('/')+'/'+q_name,'r') as f:
+	with open(os.path.join(tmp_dir,q_name),'r') as f:
 		for l in f:
-			print(l)
+			print(l,end='')
 
 def case_view():
+	print("Launching {:s}..".format(player))
 	return view_vid(tmp_dir)
-
-#remove_top(q_name,tmp_dir)
-#print(view_vid(tmp_dir))
-#add_bottom(q_name,tmp_dir,"my_line")
-#print(yt_dl_bin("https://www.youtube.com/watch?v=so_Ib0ErkQU",tmp_dir))
-#case_default()
-
-if len(sys.argv)<2:
-	# 0th is name of script
-	# 1th+ are the options
-	sys.exit(1)
 
 ops0 = {	#keyword function req_args
 	"view": case_view,
@@ -150,9 +148,14 @@ ops1 = {
 	"go"  : case_go, #1
 	"add" : case_add, #1
 }
+if len(sys.argv)<2:
+	ops0["*"]()
+	sys.exit(0)
 cmd = sys.argv[1].lower()
 if cmd in ops0:
 	ops0[cmd]()
 elif cmd in ops1:
 	ops1[cmd](sys.argv[2])
+else:
+	sys.exit(2)
 
