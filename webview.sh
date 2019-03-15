7629b7776c6137607fd7ca04ff2ba27a6b08df74
Perry Eng <eng.perry@gmail.com>
2019-03-15 16:26:21 -0500

    Removed redundent url echo in fq9 add. webview.sh now allows default arguments for designated viewers.
    Weird bug (, but not mines?):
    	feh's '-k' option saves the image to the current
    	directory and not /tmp as it says. Seems like no
    	one else has this problem so might be how I'm
    	calling it.

--- webview.sh
+++ webview.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-[ "$#" -eq 0 ] && return 1
+[ $# -eq 0 ] && return 1
 
 bin=
+def_args=
 case "$1" in
 	*.jpg|*.jpeg|*.png|*.gif) # image exts
 		bin=feh
+		def_args='-x -k -q'
 	;;
 	*.mp4|*.avi|*.flv) # video exts
 		bin=mpv
+		def_args='--pause --keep-open --really-quiet'
 	;;
 	https://www.youtube.com/watch?v=*|*.crunchyroll.com/*)
 		bin='./fq9 add'
@@ -16,4 +19,4 @@ case "$1" in
 		bin=xlinks2
 	;;
 esac
-exec $bin $1
+exec $bin $def_args $1
