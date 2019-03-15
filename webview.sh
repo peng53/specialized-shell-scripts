df404ff3998fd4b01b1dd52f6323f8bac4e4e1f1
Perry Eng <eng.perry@gmail.com>
2019-03-15 15:59:57 -0500

    Changed order of priority so that images from youtube/crunchyroll are opened in feh rather than added the queue.

--- webview.sh
+++ webview.sh
@@ -3,14 +3,14 @@
 
 bin=
 case "$1" in
-	https://www.youtube.com/watch?v=*)
-		bin='./fq8 add'
-	;;
 	*.jpg|*.jpeg|*.png|*.gif) # image exts
 		bin=feh
 	;;
 	*.mp4|*.avi|*.flv) # video exts
 		bin=mpv
+	;;
+	https://www.youtube.com/watch?v=*|*.crunchyroll.com/*)
+		bin='./fq9 add'
 	;;
 	*) # the default option
 		bin=xlinks2
