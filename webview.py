7756a8e492bab00e261c293bc97044e5a7bfa3d4
Perry E <eng.perry@gmail.com>
2019-03-16 17:03:04 -0500

    Changed CallOption to contain the name of the option.
    Changed how the final command is called since xlinks (& prob others)
    intepret an empty args as an unknown option.
    e.g. `cmd ' ' arg`

--- webview.py
+++ webview.py
@@ -14,7 +14,8 @@ def matches_file_ext(filename: str, exts: List[str]) -> bool:
 	return any(filename.endswith(ext) for ext in exts)
 
 class CallOption:
-	def __init__(self, test: Callable[[str],bool], action: str, args: str = ''):
+	def __init__(self, name: str, test: Callable[[str],bool], action: str, args: str = None):
+		self.name = name
 		self.test = test
 		self.action = action
 		self.args = args
@@ -25,23 +26,34 @@ import os
 alias = {
 	"fq9" : os.path.join(os.path.dirname(os.path.abspath(__file__)),"fq9")
 }
-call_type = {
-	"image" : CallOption(lambda p: matches_file_ext(p, ['.jpg','.jpeg','.png','.gif']),"feh"),
-	"video" : CallOption(lambda p: matches_file_ext(p, ['.mp4','.avi','.flv']),"mpv"),
-	"youtube" : CallOption(lambda p: "youtube.com/watch?v=" in p, alias["fq9"], "add"),
-	"crunchyroll" : CallOption(lambda p: ".crunchyroll.com/" in p, alias["fq9"], "add")
-}
+
+call_type = [
+	CallOption(
+		"image",
+		lambda p: matches_file_ext(p, ['.jpg','.jpeg','.png','.gif']),
+		"feh"),
+	CallOption(
+		"video",
+		lambda p: matches_file_ext(p, ['.mp4','.avi','.flv']),
+		"mpv"),
+	CallOption(
+		"youtube",
+		lambda p: "youtube.com/watch?v=" in p,
+		alias["fq9"],
+		"add"),
+	CallOption(
+		"crunchyroll",
+		lambda p: ".crunchyroll.com/" in p,
+		alias["fq9"],
+		"add"),
+]
 
 url = sys.argv[1] # type: str
-action = None
-args = None
+cmd = "xlinks2" # type: str
 
 for t in call_type:
-	if call_type[t].test(url):
-		action, args = call_type[t].action, call_type[t].args
+	if t.test(url):
+		cmd = t.action+' '+t.args if t.args else t.action # type: str
 		break
 
-if not action:
-	action = "xlinks2"
-
-Popen([action, args, url])
+Popen([cmd, url])
