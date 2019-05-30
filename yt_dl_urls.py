fa28c828607e50de269d2fa05620714501f32cd4
Perry E <eng.perry@gmail.com>
2019-05-30 12:42:52 -0500

    Working prototype, call like so:
    python3 yt_dl_urls URL HEIGHT [WEBM|MP4] ABR
    
    ATM it saves both video and audio to
    /mnt/ramdisk/1 and 1aud
    and will fail if they already exists or the directory
    does not exists.
    
    What this is is a script that retrieves URLs from youtube
    using youtube-dl, and then downloads the video and audio
    with threads and then plays the result. For now I only
    know that the downloading works for sure.
    
    Script needs much work

--- yt_dl_urls.py
+++ yt_dl_urls.py
@@ -1,5 +1,12 @@
 import youtube_dl
 import functools
+import os
+import urllib
+import shutil
+import threading
+import subprocess
+from time import sleep
+
 def get_urls(url: str, vheight: int, vfmt: str, abr: int):
 	"""
 	Uses youtube-dl's api to get urls matching the video
@@ -12,22 +19,32 @@ def get_urls(url: str, vheight: int, vfmt: str, abr: int):
 	}
 	ydl = youtube_dl.YoutubeDL(args)
 	dd  = ydl.extract_info(url, download=False)
-	vid = filter(lambda d: d["ext"]==vfmt, dd["formats"])
+	vid = filter(lambda d: d['vcodec']!='none' and d["ext"]==vfmt, dd["formats"])
 	vmatch = next(filter(lambda v: int(v["height"])==vheight, vid), None)
 	if not vmatch:
 		print("no vmatch")
 		return
-	aud = filter(lambda d: d["format_note"]=='DASH audio', dd["formats"])
+	aud = filter(lambda d: d['vcodec']=='none', dd["formats"])
 	amatches = filter(lambda a: a["abr"]<=abr, aud)
 	abest = functools.reduce(lambda x,y: x if x["abr"]>y["abr"] else y, amatches)
 	if abest:
 		return (vmatch['url'],abest['url'])
 	print("no amatch")
 
+def download(url: str, outdir: str, filename: str):
+	# Downloads Url to outdir/filename.
+	# Will not clobber.
+	path = os.path.join(outdir,filename)
+	if os.path.exists(path):
+		return
+	with urllib.request.urlopen(url) as response, open(path, 'wb') as out_file:
+		shutil.copyfileobj(response, out_file)
+
 if __name__=='__main__':
 	from sys import argv
 	if len(argv)>=5:
 		url, vheight, fmt, abr = argv[1:]
+		outdir = '/mnt/ramdisk/'
 		try:
 			vheight, abr = int(vheight), int(abr)
 		except:
@@ -38,7 +55,16 @@ if __name__=='__main__':
 			fmt,
 			abr
 		)
-		print(r)
 		if r:
-			print(r[0])
-			print(r[1])
+			print("Starting Download")
+			vidt = threading.Thread(target=download, args = (r[0], outdir, '1'))
+			vidt.start()
+			audt = threading.Thread(target=download, args = (r[1], outdir, '1aud'))
+			audt.start()
+			print("Waiting to play")
+			sleep(10)
+			vidf = os.path.join(outdir,'1')
+			audf = os.path.join(outdir,'1aud')
+			subprocess.run(['mpv', vidf, '--audio-file='+audf])
+			vidt.join()
+			audt.join()
