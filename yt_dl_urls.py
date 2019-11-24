ad8831b70e40e484ab9b2aa98086fd9dae3a2459
Perry E. <eng.perry@gmail.com>
2019-11-23 20:00:39 -0600

    Overhaul of yt_dl_urls 'mainloop' Instead of just downloading, it can alternatively check existence of format(s)
    Instead of my own argument parsing, now used argparse with good help descriptions
    (python3 yt_dl_urls -h)
    
    Instead of using urllib to download, uses youtube_dl.
    
    Legacy code still needs to be deleted, ratelimit should be an argument,
    parts of 'mainloop' should be partitioned

--- yt_dl_urls.py
+++ yt_dl_urls.py
@@ -32,9 +32,9 @@ def get_urls(url: str, vheight: int, vfmt: str, abr: int):
 	print("no amatch")
 
 def get_video_fmt(info, vheight: int, vfmt: str):
-	vids = filter(lambda d: d['vcodec']!='none' d['acodec']=='none' and d['ext']==vfmt, info['formats'])
+	vids = filter(lambda d: d['vcodec']!='none' and d['acodec']=='none' and d['ext']==vfmt, info['formats'])
 	vmatch = next(filter(lambda v: int(v['height'])==vheight, vids), None)
-	return vmatch
+	return vmatch['format_id'] if vmatch else None
 
 def get_audio_fmt(info, abr: int):
 	# Returns audio fmt with abr equal or less than desired abr
@@ -53,31 +53,84 @@ def download(url: str, outdir: str, filename: str):
 	with urllib.request.urlopen(url) as response, open(path, 'wb') as out_file:
 		shutil.copyfileobj(response, out_file)
 
+def download_fmt(url: str, fmt: int, out: str, ratelimit=None):
+	if os.path.exists(out):
+		raise FileAlreadyExistsException
+	args = {
+		"call_home": False,
+		"quiet": True,
+		'format': fmt,
+		'outtmpl': out,
+		'nopart': True,
+	}
+	if ratelimit:
+		args['ratelimit'] = ratelimit*1000
+	downloader = youtube_dl.YoutubeDL(args)
+	#downloader.extract_info(url)
+	downloader.download([url])
+
+def exist_fmt(args):
+	pass
+
 if __name__=='__main__':
+	import argparse
+	parser = argparse.ArgumentParser('Perform certain tasks with youtube-dl api')
+	vidparams = parser.add_argument_group('Video Params', description='params that describe the desired video.')
+	parser.add_argument('task', type=str, help='Task to be completed with URL', choices=['exists', 'download'])
+	vidparams.add_argument('url', type=str, help='Video URL')
+	vidparams.add_argument('-F', '--fmt', type=str, help='File format', default='webm')
+	vidparams.add_argument('-r', '--res', type=int, help='Video vertical resolution')
+	vidparams.add_argument('-a', '--abr', type=int, help='Max audio bitrate')
+	vidparams.add_argument('-o', '--out', type=str, help='Download directory', default=os.path.join(os.getcwd(),'out'))
+
 	from sys import argv
-	if len(argv)>=5:
-		url, vheight, fmt, abr = argv[1:]
-		outdir = '/mnt/ramdisk/'
-		try:
-			vheight, abr = int(vheight), int(abr)
-		except:
-			exit()
-		r = get_urls(
-			url,
-			vheight,
-			fmt,
-			abr
-		)
-		if r:
-			print("Starting Download")
-			vidt = threading.Thread(target=download, args = (r[0], outdir, '1'))
-			vidt.start()
-			audt = threading.Thread(target=download, args = (r[1], outdir, '1aud'))
-			audt.start()
-			print("Waiting to play")
-			sleep(10)
-			vidf = os.path.join(outdir,'1')
-			audf = os.path.join(outdir,'1aud')
-			subprocess.run(['mpv', vidf, '--audio-file='+audf])
-			vidt.join()
-			audt.join()
+
+	t = parser.parse_args(argv[1:])
+	yd =youtube_dl.YoutubeDL(
+		{
+		"call_home": False,
+		"quiet": True,
+		'skip_download': True
+		}
+	)
+	vidf = audf = None
+	info = yd.extract_info(t.url)
+	if t.res:
+		vidf = get_video_fmt(info, t.res, t.fmt)
+	if t.abr:
+		audf = get_audio_fmt(info, t.abr)
+	success = True if ((not t.res or vidf) and (not t.abr or audf)) else False
+	if t.task=='exists':
+		print(success)
+	elif t.task=='download' and success:
+		threads = []
+		if t.res:
+			threads.append(
+				threading.Thread(
+					target=download_fmt,
+					args=(
+						t.url,
+						vidf,
+						t.out,
+						40
+					)
+				)
+			)
+		if t.abr:
+			threads.append(
+				threading.Thread(
+					target=download_fmt,
+					args=(
+						t.url,
+						audf,
+						t.out+'aud',
+						40
+					)
+				)
+			)
+		for t in threads:
+			print("Started!")
+			t.start()
+		for t in threads:
+			print("Ended!")
+			t.join()
