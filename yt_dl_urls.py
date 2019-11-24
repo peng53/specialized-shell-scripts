efb4fc6877a1aebe6977eec5eadd10eb99d5fdbc
Perry E. <eng.perry@gmail.com>
2019-11-23 20:13:04 -0600

    Refactor:   moved parser creation to object,   moved yt 'info object' creation to function,   changed vidf & audf to use ternary form,   deleted some legacy functions

--- yt_dl_urls.py
+++ yt_dl_urls.py
@@ -7,29 +7,14 @@ import threading
 import subprocess
 from time import sleep
 
-def get_urls(url: str, vheight: int, vfmt: str, abr: int):
-	"""
-	Uses youtube-dl's api to get urls matching the video
-	resolution and audio bitrate.
-	"""
-	args = {
+def yt_info():
+	return youtube_dl.YoutubeDL(
+		{
 		"call_home": False,
-		"list_formats": True,
-		"quiet": True
-	}
-	ydl = youtube_dl.YoutubeDL(args)
-	dd  = ydl.extract_info(url, download=False)
-	vid = filter(lambda d: d['vcodec']!='none' and d["ext"]==vfmt, dd["formats"])
-	vmatch = next(filter(lambda v: int(v["height"])==vheight, vid), None)
-	if not vmatch:
-		print("no vmatch")
-		return
-	aud = filter(lambda d: d['vcodec']=='none', dd["formats"])
-	amatches = filter(lambda a: a["abr"]<=abr, aud)
-	abest = functools.reduce(lambda x,y: x if x["abr"]>y["abr"] else y, amatches)
-	if abest:
-		return (vmatch['url'],abest['url'])
-	print("no amatch")
+		"quiet": True,
+		'skip_download': True
+		}
+	)
 
 def get_video_fmt(info, vheight: int, vfmt: str):
 	vids = filter(lambda d: d['vcodec']!='none' and d['acodec']=='none' and d['ext']==vfmt, info['formats'])
@@ -66,40 +51,36 @@ def download_fmt(url: str, fmt: int, out: str, ratelimit=None):
 	if ratelimit:
 		args['ratelimit'] = ratelimit*1000
 	downloader = youtube_dl.YoutubeDL(args)
-	#downloader.extract_info(url)
 	downloader.download([url])
 
-def exist_fmt(args):
-	pass
+import argparse
+
+class MainParser:
+	def __init__(self):
+		self.parser = argparse.ArgumentParser('Perform certain tasks with youtube-dl api')
+		self.vidparams = self.parser.add_argument_group('Video Params', description='params that describe the desired video.')
+		self.parser.add_argument('task', type=str, help='Task to be completed with URL', choices=['exists', 'download'])
+		self.vidparams.add_argument('url', type=str, help='Video URL')
+		self.vidparams.add_argument('-F', '--fmt', type=str, help='File format', default='webm')
+		self.vidparams.add_argument('--res', type=int, help='Video vertical resolution')
+		self.vidparams.add_argument('-a', '--abr', type=int, help='Max audio bitrate')
+
+		self.vidparams.add_argument('-o', '--out', type=str, help='Download directory', default=os.path.join(os.getcwd(),'out'))
+		self.vidparams.add_argument('-r', '--rate', type=int, help='Download rate in KB/S', default=None)
+	def parse(self, args):
+		return self.parser.parse_args(args)
+
+from sys import argv
 
 if __name__=='__main__':
-	import argparse
-	parser = argparse.ArgumentParser('Perform certain tasks with youtube-dl api')
-	vidparams = parser.add_argument_group('Video Params', description='params that describe the desired video.')
-	parser.add_argument('task', type=str, help='Task to be completed with URL', choices=['exists', 'download'])
-	vidparams.add_argument('url', type=str, help='Video URL')
-	vidparams.add_argument('-F', '--fmt', type=str, help='File format', default='webm')
-	vidparams.add_argument('-r', '--res', type=int, help='Video vertical resolution')
-	vidparams.add_argument('-a', '--abr', type=int, help='Max audio bitrate')
-	vidparams.add_argument('-o', '--out', type=str, help='Download directory', default=os.path.join(os.getcwd(),'out'))
-
-	from sys import argv
-
-	t = parser.parse_args(argv[1:])
-	yd =youtube_dl.YoutubeDL(
-		{
-		"call_home": False,
-		"quiet": True,
-		'skip_download': True
-		}
-	)
-	vidf = audf = None
+	t = MainParser().parse(argv[1:])
+	yd = yt_info()
 	info = yd.extract_info(t.url)
-	if t.res:
-		vidf = get_video_fmt(info, t.res, t.fmt)
-	if t.abr:
-		audf = get_audio_fmt(info, t.abr)
+
+	vidf = get_video_fmt(info, t.res, t.fmt) if t.res else None
+	audf = get_audio_fmt(info, t.abr) if t.abr else None
 	success = True if ((not t.res or vidf) and (not t.abr or audf)) else False
+
 	if t.task=='exists':
 		print(success)
 	elif t.task=='download' and success:
