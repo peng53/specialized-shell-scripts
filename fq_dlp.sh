291eeab093b02f9be15df4c7cda78f68aaf1fcb6
Perry Eng <eng.perry@gmail.com>
2023-08-19 21:41:37 -0500

    Inital add fq_dlp.sh Work on using yt-dlp in the manner similar to before. Difference is no more output parsing for format codes. Under going rebuild, currently just the download function and view function- can't even cleanup after itself

---
+++ fq_dlp.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# fq_dlp.sh / fq rewrite with yt-dlp instead of youtube-dl
+
+readonly tempDirectory=/mnt/ramdisk/
+readonly urlQueue=${tempDirectory}mq
+readonly urlQueueHist=${tempDirectory}q_hist
+readonly lastPids=${tempDirectory}last_pids
+readonly playerDefaultArgs='--pause --keep-open --really-quiet'
+readonly yt_dlp="~/bin/yt-dlp"
+
+# if setting any of the below in shell, remember to export them
+readonly vres=${vres:-240}
+readonly vext=${vext:-webm}
+readonly abr=${abr:-80}
+readonly speed=${speed:-48}
+
+
+downloadYoutube(){
+	local url=$1
+	local out=$tempDictory/yt-vid
+	echo "Downloading video at ${vres}p $vext format with ${abr}k audio"
+	eval "$yt_dlp -S "res:$vres,vext:$vext,abr:$abr,size" --no-playlist -r $speed'k' -o $out -c --no-part $url" &
+}
+viewMedia(){
+	if [ -s ${tempDirectory}/yt-vid ]
+	then
+		echo Viewing media
+		mpv $playerDefaultArgs ${tempDirectory}/yt-vid &
+	fi
+}
+
+case "$1" in
+	'')
+		echo 'No arguments..'
+	;;
+	view)
+		viewMedia
+	;;
+	*)
+		echo "Downloading: $1"
+		downloadYoutube $1
+		sleep 5
+		viewMedia
+	;;
+esac
